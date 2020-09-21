e68b689fad0fada41d7d62acf5c37cb24bd403a4
Diego-ex <diego.pobletesanhueza@gmail.com>
2020-09-21 12:14:23 -0300

    Segundo Commit: Inicio de práctica de contenedores Spinner y Recycler View. Creación de dos Screen para ambos botones agregados y programados para mostrar las dos pantallas.

--- app/src/main/java/com/poblete/contenedores/MainActivity.java
+++ app/src/main/java/com/poblete/contenedores/MainActivity.java
@@ -2,13 +2,45 @@ package com.poblete.contenedores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn_Spinner, btn_RecylerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_Spinner = findViewById(R.id.btn_Spinner);
+        btn_RecylerView = findViewById(R.id.btn_RecyclerView);
+
+        // Para cambiar de pantalla se debe crear un objeto de la clase Intent
+
+        btn_Spinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // se deben ingresar dos parametros, el activity actual y el activity que quiero mostrar
+                Intent intento = new Intent(MainActivity.this, SpinnerActivity.class);
+                startActivity(intento);
+                // toast de prueba Toast.makeText(MainActivity.this, "Spinner", Toast.LENGTH_LONG).show();
+            }
+        });
+        btn_RecylerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                // toast de prueba Toast.makeText(MainActivity.this, "Recyler View", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
